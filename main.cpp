b46c3aee59fa2eebf17cb2597032f6ee16fa56de
Aly Osama <alyosamah@gmail.com>
2015-11-20 16:51:03 +0200

    Adding Reading and Writting Packets from files  mode

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <PcapLiveDeviceList.h>
 #include <PcapLiveDevice.h>
+#include <PcapFileDevice.h>
+#include <PlatformSpecificUtils.h>
 #include <RawPacket.h>
 #include <Packet.h>
 #include <IpAddress.h>
@@ -31,17 +33,12 @@
 #include <fstream>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
+void printPacket(RawPacket* rawPacket){
 
-int count=0;
-
-void packetRecieved(RawPacket* rawPacket, PcapLiveDevice* pDevice,void* userCookie)
-{
-    printf("packet received %d\n",count++);
-
-    ////parsing
     Packet packet(rawPacket);
     // Print packet types
     printf("Packet is of type: ");
@@ -85,9 +82,88 @@ void packetRecieved(RawPacket* rawPacket, PcapLiveDevice* pDevice,void* userCook
 
     }
     printf("\n");
+}
+
+
+int count=0;
+void packetRecieved(RawPacket* rawPacket, PcapLiveDevice* pDevice,void* userCookie)
+{
+    printf("packet received %d\n",++count);
+    printPacket(rawPacket);
 
 
 }
+void savePacketsToFile(const char* fileName,RawPacketVector& packets, char* errString)
+{
+    PcapFileWriterDevice writerDevice(fileName);
+    if (!writerDevice.open())
+    {
+        printf("Error opening writer device for %s: %s", fileName, errString);
+        return;
+    }
+
+    for (RawPacketVector::VectorIterator packetIter = packets.begin(); packetIter != packets.end(); packetIter++)
+    {
+        writerDevice.writePacket(*(*packetIter));
+    }
+
+    writerDevice.close();
+}
+
+void readPacketsFromFile(string filename,RawPacketVector& packets, char* errorString){
+
+    PcapFileReaderDevice readerDevice(filename.c_str());
+    if (!readerDevice.open())
+    {
+        printf("Error opening the device: %s", errorString);
+        return;
+    }
+    RawPacket rawPacket;
+    while (readerDevice.getNextPacket(rawPacket))
+    {
+        packets.pushBack(&rawPacket);
+    }
+    readerDevice.close();
+
+}
+
+void readPackets(string filename){
+
+    printf("Start Reading Packets From %s\n",filename.c_str());
+    char errorString[1000];
+    LoggerPP::getInstance().setErrorString(errorString, 1000);
+    RawPacketVector packets;
+    readPacketsFromFile(filename,packets,errorString);
+
+    printf("Number of Packets found: %d\n", packets.size());
+    int i = 0;
+    for (RawPacketVector::VectorIterator packetIter = packets.begin(); packetIter != packets.end(); packetIter++)
+    {
+        printf("packet read %d\n",++i);
+        printPacket(*packetIter);
+    }
+
+
+}
+
+void savePackets(PcapLiveDevice *pDevice,string filename,int time){
+    printf("Start Saving Packets at %s\n",filename.c_str());
+    char errorString[1000];
+    LoggerPP::getInstance().setErrorString(errorString, 1000);
+    RawPacketVector capturedPackets;
+    pDevice->startCapture(capturedPackets);
+    PCAP_SLEEP(time);
+    pDevice->stopCapture();
+    savePacketsToFile(filename.c_str(),capturedPackets,errorString);
+    printf("End Saving %d Packets\n",capturedPackets.size());
+}
+
+void livePackets(PcapLiveDevice *pDevice,int time){
+    pDevice->startCapture(packetRecieved, NULL);
+    PCAP_SLEEP(time);
+    pDevice->stopCapture();
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -105,8 +181,25 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 
-    pIfaceDevice->startCapture(packetRecieved, NULL);
-    while(true){}
+
+    // Options
+    char mode='R';
+    int time=10;
+    string filename="p.pcap";
+
+    switch(mode){
+    case 'L':
+        livePackets(pIfaceDevice,time);
+        break;
+    case 'S':
+        savePackets(pIfaceDevice,filename,time);
+        break;
+    case 'R':
+        readPackets(filename);
+    default:
+        break;
+    }
+
     pIfaceDevice->close();
 
 }
